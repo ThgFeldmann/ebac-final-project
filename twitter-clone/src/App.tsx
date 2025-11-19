ae82097efb378fc4741bcb172e1a8ff34913119a
Thiago <thiagofeldmann06@gmail.com>
2025-11-19 11:01:55 -0300

    update: api urls

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -13,30 +13,30 @@ import { GlobalStyle } from "./styles";
 
 // this is the URL for 'Users' section
 export const apiUsers = {
-  Get: "http://127.0.0.1:8000/api/users/get/",
-  Create: "http://127.0.0.1:8000/api/users/create/",
-  Delete: "http://127.0.0.1:8000/api/users/delete/"
+  Get: "https://echo-back-end.vercel.app//api/users/get/",
+  Create: "https://echo-back-end.vercel.app//api/users/create/",
+  Delete: "https://echo-back-end.vercel.app//api/users/delete/"
 }
 
 // this is the URL for 'Posts' section
 export const apiPosts = {
-  Get: "http://127.0.0.1:8000/api/posts/get/",
-  Create: "http://127.0.0.1:8000/api/posts/create/",
-  Delete: "http://127.0.0.1:8000/api/posts/delete/"
+  Get: "https://echo-back-end.vercel.app//api/posts/get/",
+  Create: "https://echo-back-end.vercel.app//api/posts/create/",
+  Delete: "https://echo-back-end.vercel.app//api/posts/delete/"
 }
 
 //this is the URL for 'Comment' section
 export const apiComments = {
-  Get: "http://127.0.0.1:8000/api/comments/get/",
-  Create: "http://127.0.0.1:8000/api/comments/create/",
-  Delete: "http://127.0.0.1:8000/api/comments/delete/"
+  Get: "https://echo-back-end.vercel.app//api/comments/get/",
+  Create: "https://echo-back-end.vercel.app//api/comments/create/",
+  Delete: "https://echo-back-end.vercel.app//api/comments/delete/"
 }
 
 // this is the URL for 'Follows' section
 export const apiFollows = {
-  Get: "http://127.0.0.1:8000/api/follows/get/",
-  Create: "http://127.0.0.1:8000/api/follows/create/",
-  Delete: "http://127.0.0.1:8000/api/follows/delete/"
+  Get: "https://echo-back-end.vercel.app//api/follows/get/",
+  Create: "https://echo-back-end.vercel.app//api/follows/create/",
+  Delete: "https://echo-back-end.vercel.app//api/follows/delete/"
 }
 
 // Type of User
