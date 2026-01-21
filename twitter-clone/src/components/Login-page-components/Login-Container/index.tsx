a978188b7a2a73fbcda396115423ecb7ebcbfd3e
Thiago <thiagofeldmann06@gmail.com>
2026-01-21 14:02:49 -0300

    wip: old functions

--- twitter-clone/src/components/Login-page-components/Login-Container/index.tsx
+++ twitter-clone/src/components/Login-page-components/Login-Container/index.tsx
@@ -1,3 +1,5 @@
+//TODO fix response being delayed compared to the other functions
+
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 
@@ -14,8 +16,8 @@ const LoginContainerComponent = () => {
     // Holds informations typed on the form
     const [formData, setFormData] = useState<FormValues>({email: '', password: ''}) 
 
-    // Holds information on every user
-    const [usersList, setUsersList] = useState<User[]>([])
+    // // Holds information on every user
+    // const [usersList, setUsersList] = useState<User[]>([])
 
     // Stores user information in case login is a success
     const [loggedUser, setLoggedUser] = useState<User>({
@@ -55,79 +57,78 @@ const LoginContainerComponent = () => {
         }
     }
 
-    const GetUsersList = async () => {
-        console.log("Starting request...")
-        await fetch(apiUsers.Get)
-            .then((response) => response.json())
-            .then((response: User[]) => {
-                console.log("response: ", response)
-                setUsersList(response)
-            })
-    }
+    // const GetUsersList = () => {
+    //     console.log("Starting request...")
+    //     fetch(apiUsers.Get)
+    //         .then((response) => response.json())
+    //         .then((response: User[]) => {
+    //             console.log("response: ", response)
+    //             setUsersList(response)
+    //         })
+    // }
 
-    const FilterUser = (usersList: User[]) => {
-        console.log("Users list: ", usersList)
+    // const FilterUser = (usersList: User[]) => {
+    //     console.log("Users list: ", usersList)
 
-        console.log("Filtering user...")
-        const user = usersList.find(
-            (u: User) =>
-                u.email === formData.email 
-                &&
-                u.password === formData.password
-        )
+    //     console.log("Filtering user...")
+    //     const user = usersList.find(
+    //         (u: User) =>
+    //             u.email === formData.email 
+    //             &&
+    //             u.password === formData.password
+    //     )
 
-        console.log("user: ", user)
+    //     console.log("user: ", user)
 
-        if (user) {
-            console.log("Login successfull...")
-            setLoggedUser(user)
-            setSuccess(true)
-        } else {
-            console.log("Invalid Credentials")
-            setFormError(true)
-        }
-    }
+    //     if (user) {
+    //         console.log("Login successfull...")
+    //         setLoggedUser(user)
+    //         setSuccess(true)
+    //     } else {
+    //         console.log("Invalid Credentials")
+    //         setFormError(true)
+    //     }
+    // }
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
-
-        GetUsersList()
-        FilterUser(usersList)
-    }
-
-    // // executes all submit functions and checks if successful
     // const handleSubmit = (e: any) => {
     //     e.preventDefault()
 
-    //     console.log("Starting the request...")
-    //     fetch(apiUsers.Get)
-    //         .then((response) => response.json())
-    //         .then((users) => {
-
-    //             console.log("Request response: ", users)
-    //             //? maybe remove verifications from the request?
-
-    //             console.log("Filtering user...")
-    //             const user: User = users.find(
-    //                 (u: User) =>
-    //                     u.email === formData.email 
-    //                     &&
-    //                     u.password === formData.password
-    //             )
-
-    //             console.log("User: ", user)
-
-    //             console.log("Validating user...")
-    //             if (user) {
-    //                 console.log("Login successful!")
-    //                 setLoggedUser(user)
-    //                 setSuccess(true)
-    //             } else {
-    //                 console.log("Invalid Credentials")
-    //                 setFormError(true)
-    //             }
-    //         })
+    //     GetUsersList()
+    //     FilterUser(usersList)
     // }
+
+    // executes all submit functions and checks if successful
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+
+        console.log("Starting the request...")
+        fetch(apiUsers.Get)
+            .then((response) => response.json())
+            .then((users) => {
+
+                console.log("Request response: ", users)
+
+                console.log("Filtering user...")
+                const user: User = users.find(
+                    (u: User) =>
+                        u.email === formData.email 
+                        &&
+                        u.password === formData.password
+                )
+
+                console.log("User: ", user)
+
+                console.log("Validating user...")
+                if (user) {
+                    console.log("Login successful!")
+                    setLoggedUser(user)
+                    setSuccess(true)
+                } else {
+                    console.log("Invalid Credentials")
+                    setFormError(true)
+                }
+            })
+    }
 
     //* handles the navigation, also sends the user data to the home page
     const handleNavigate = async (user: User) => {
@@ -139,7 +140,7 @@ const LoginContainerComponent = () => {
     useEffect(() => {
         setSuccess(false)
         setFormData({email: '', password: ''})
-        setUsersList([])
+        // setUsersList([])
         setLoggedUser({
             id: 0,
             username: '',
