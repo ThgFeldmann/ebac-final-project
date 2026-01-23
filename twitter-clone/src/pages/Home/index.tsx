6a981b2b816f903ca8fe105f77da5988e85ba1ca
Thiago <thiagofeldmann06@gmail.com>
2026-01-23 14:02:18 -0300

    add: debug message on home page

--- twitter-clone/src/pages/Home/index.tsx
+++ twitter-clone/src/pages/Home/index.tsx
@@ -106,6 +106,8 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    console.log("Comment list on home page: ", CommentList)
+
     return (
         <>
             <ModalOverlay onClick={e => removeOverlay()} className={handleOverlayDisplay()} />
